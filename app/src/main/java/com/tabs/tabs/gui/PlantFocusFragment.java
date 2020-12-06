74786b1e4cfb702ebc42a0dfc1b515e514305e03
Tyler Jang <taj26@duke.edu>
2020-12-06 11:47:53 -0700

    focusable unfocus

--- app/src/main/java/com/tabs/tabs/gui/PlantFocusFragment.java
+++ app/src/main/java/com/tabs/tabs/gui/PlantFocusFragment.java
@@ -1,13 +1,17 @@
 package com.tabs.tabs.gui;
 
+import android.content.Context;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -15,10 +19,13 @@ import androidx.fragment.app.Fragment;
 import com.tabs.tabs.R;
 import com.tabs.tabs.plants.Plant;
 
+import java.sql.SQLOutput;
+
 public class PlantFocusFragment extends Fragment {
 
     private Plant plant;
     private ImageView img;
+    private RelativeLayout parentCard;
     private EditText name;
     private EditText subtitle;
     private ViewGroup rootView;
@@ -33,6 +40,27 @@ public class PlantFocusFragment extends Fragment {
         rootView = (ViewGroup) inflater.inflate(R.layout.plant_focus_fragment, container, false);
         img = rootView.findViewById(R.id.img);
         name = rootView.findViewById(R.id.name);
+        parentCard = rootView.findViewById(R.id.plantcard);
+
+        parentCard.setOnClickListener(s->{
+            parentCard.setVisibility(View.GONE);
+            parentCard.setVisibility(View.VISIBLE);
+
+            System.out.println("I was hiding");
+//            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+
+            View view = getActivity().getCurrentFocus();
+            System.out.println("My view: " + view);
+//            if (view != null) {
+//                InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+//                imm.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
+//            } else {
+            InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
+
+
+        });
 
         subtitle = rootView.findViewById(R.id.subtitle);
         //ImageView prof = rootView.findViewById(R.id.profile_pic);
@@ -50,17 +78,32 @@ public class PlantFocusFragment extends Fragment {
     }
 
     public void draw(String s) {
+
         img.setImageResource(getContext().getResources().getIdentifier(plant.getFileName(), "drawable", getContext().getPackageName()));
         name.setText(plant.getProfile().getName());
         if(name.requestFocus()) {
-            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         }
         name.setOnFocusChangeListener((v, hasFocus)-> {
-            if (!hasFocus) {
+          if (!hasFocus) {
+//            name.requestFocus();
+
                 plant.getProfile().setName(name.getText().toString());
-                getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-            }
+                getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+              System.out.println("I have focus");
+
+//              name.clearFocus();
+//              name.setShowSoftInputOnFocus(false);
+          } else {
+              System.out.println("I lack focus");
+//              name.clearFocus();
+//              name.setShowSoftInputOnFocus(false);
+          }
         });
+
+
+
         subtitle.setText(plant.getProfile().getSubtitle());
         if(subtitle.requestFocus()) {
             getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
